e1a3930fa6efd9e51d407ad4845fa126310e4eab
Owen <owenpotent@gmail.com>
2024-05-27 16:40:21 +0000

    chore: Refactor ban command to handle edge cases and improve error handling

--- src/commands/moderation/ban.ts
+++ src/commands/moderation/ban.ts
@@ -37,6 +37,77 @@ export default <BaseCommand>{
         const user = options.getMember("user") as GuildMember;
         const reason = options.getString("reason") as string;
 
+        if (!interaction.guild) return;
 
+        if (!user.bannable) {
+            return await interaction.reply({
+                content: moderation.ban.unableToBan.replace("{user}", user.user.username),
+                ephemeral: true
+            });
+        }
+
+        if (user.id === interaction.guild.ownerId) {
+            return await interaction.reply({
+                content: moderation.ban.unableToBanOwner,
+                ephemeral: true
+            });
+        }
+
+        if (user.id === interaction.user.id) {
+            return await interaction.reply({
+                content: moderation.ban.selfBan,
+                ephemeral: true
+            });
+        }
+
+        if (user.roles.highest.position >= (interaction.member as GuildMember).roles.highest.position) {
+            return await interaction.reply({
+                content: moderation.ban.highestRole,
+                ephemeral: true
+            });
+        }
+
+        try {
+            const embed = new EmbedBuilder()
+                .setTitle("User Banned")
+                .setColor("Aqua")
+                .setFooter({
+                    text: moderation.ban.success.footer.replace("{MODERATOR}", user.user.username),
+                })
+                .setTimestamp()
+
+            if (reason) {
+                embed.setDescription(moderation.ban.success.reason.description.replace("{REASON}", reason));
+
+                await user.ban({ reason: reason });
+                await interaction.reply({ embeds: [embed] });
+
+                try {
+                    await user.send({
+                        content: moderation.ban.success.dm.replace("{REASON}", reason).replace("{GUILD}", interaction.guild.name)                    });
+                } catch (error) {
+                    Logger.error("Failed to send DM to user.");
+                }
+            } else {
+                embed.setDescription(moderation.ban.success.noReason.description);
+
+                await user.ban();
+                await interaction.reply({ embeds: [embed] });
+
+                try {
+                    await user.send({
+                        content: moderation.ban.success.dm.replace("{REASON}", "No reason provided.").replace("{GUILD}", interaction.guild.name)
+                    });
+                } catch (error) {
+                    Logger.error("Failed to send DM to user.");
+                }
+            }
+        } catch (error) {
+            Logger.error((error as Error).message);
+            await interaction.reply({
+                content: moderation.ban.error.replace("{USER}", user.user.username),
+                ephemeral: true
+            });
+        }
     },
 };
