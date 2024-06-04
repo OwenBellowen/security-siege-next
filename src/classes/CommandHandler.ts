59e90b13122263a39a1f98fbbeb533ee22da51f7
Owen <owenpotent@gmail.com>
2024-06-04 05:08:31 +0000

    refactor: Remove commented out code and register application commands globally

--- src/classes/CommandHandler.ts
+++ src/classes/CommandHandler.ts
@@ -54,13 +54,9 @@ export default class CommandHandler {
             // Remove all global commands
             // await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [] })
             //     .then(() => Logger.info("Successfully removed all global commands."));
-
-            // Register the commands for a specific guild
+            
             // Register the commands globally
             await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
-
-            // Register the commands for a specific guild
-            // await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, guildID), { body: commands });
 
             Logger.success("Successfully registered application commands.");
         } catch (error) {
