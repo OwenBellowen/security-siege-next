8c9fe9220bab8a9e03b655d53b5cd3a18d119d70
Owen <owenpotent@gmail.com>
2024-06-16 18:53:37 +0000

    chore: Add dmUser and messageID properties to ticket setup modal

--- src/interactions/modals/ticketsetup.ts
+++ src/interactions/modals/ticketsetup.ts
@@ -30,7 +30,9 @@ export default <BaseModal> {
             startEmbed: false,
             allowedRoles: [],
             blacklistedRoles: [],
-            categories: []
+            categories: [],
+            dmUser: false,
+            messageID: ''
         });
 
         if (!ticket) {
