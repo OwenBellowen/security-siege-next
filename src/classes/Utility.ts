d21f81f3392a1d77a33fdad244dbe54dce1b1b2c
Owen <owenpotent@gmail.com>
2024-06-16 18:53:00 +0000

    chore: Update Utility class with new methods for retrieving messages and creating ticket embeds

--- src/classes/Utility.ts
+++ src/classes/Utility.ts
@@ -1,5 +1,6 @@
-import { TextChannel } from "discord.js";
+import { CommandInteraction, EmbedBuilder, TextChannel } from "discord.js";
 import BotClient from "./Client";
+import { ITicketEmbed } from "../models/TicketsModel";
 
 /**
  * Utility class with various helper methods.
@@ -43,5 +44,30 @@ export default class Utility {
      */
     public static async getChannel(channelID: string, client: BotClient): Promise<TextChannel> {
         return client.channels.cache.get(channelID) as TextChannel;
+    }
+
+    /**
+     * Retrieves a message object based on the provided channel and message IDs.
+     * @param client - The BotClient instance.
+     * @param channelID - The ID of the channel to retrieve the message from.
+     * @param messageID - The ID of the message to retrieve.
+     * @returns A Promise that resolves to a message object.
+     */
+    public static async getMessage(client: BotClient, channelID: string, messageID: string) {
+        return (await this.getChannel(channelID, client)).messages.fetch(messageID);
+    }
+
+    /**
+     * Creates a ticket embed using the provided embed model and interaction.
+     * @param embedModel - The embed model containing the title and description of the ticket embed.
+     * @param interaction - The command interaction object.
+     * @returns The created ticket embed as an instance of EmbedBuilder.
+     */
+    public static createTicketEmbed(embedModel: ITicketEmbed, interaction: CommandInteraction): EmbedBuilder {
+        return new EmbedBuilder()
+            .setTitle(embedModel.title)
+            .setDescription(embedModel.description)
+            .setColor("Aqua")
+            .setFooter({ text: 'React with the button below to start a ticket', iconURL: interaction.client.user.displayAvatarURL() });
     }
 }
